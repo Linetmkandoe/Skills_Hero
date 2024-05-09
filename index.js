eadd7624bc51947cda17b9eb22bd1ec372aeaf6a
Linet Ndau <mkandoelinet@gmail.com>
2024-05-09 19:03:32 +0300

    added cganges

--- index.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    this.classList.toggle('open');
-    document.querySelector('.navigation-menu').classList.toggle('show');
-});
